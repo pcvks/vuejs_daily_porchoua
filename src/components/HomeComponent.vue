bfa1b13a297339e7a90c58ef8e533cbe6d0fd1dc
pdev <kangserpobtsuasvaaj@gmail.com>
2024-08-04 23:45:02 -0700

    change navbar

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -13,8 +13,8 @@
                     <!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard v1</li>
+                            <li class="breadcrumb-item"><router-link>ໜ້າຫຼັກ</router-link></li>
+                            <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
                     </div>
                     <!-- /.col -->
--- src/components/PlanComponent.vue
+++ src/components/PlanComponent.vue
@@ -13,8 +13,8 @@
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard v1</li>
+                            <li class="breadcrumb-item"><router-link to="/dashboard">ໜ້າຫຼັກ</router-link></li>
+                            <li class="breadcrumb-item active">ແຜນການ</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
