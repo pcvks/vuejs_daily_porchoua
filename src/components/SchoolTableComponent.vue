34151dc58d3ce9248cd3c7f000e8add12886a817
pdev <kangserpobtsuasvaaj@gmail.com>
2024-09-18 15:24:00 -0700

    update buttondfsdsfs

--- src/components/SchoolTableComponent.vue
+++ src/components/SchoolTableComponent.vue
@@ -49,7 +49,7 @@
                 <a href="tel:+8562076589225" class="btn btn-success"><i class="fas fa-phone"></i>&nbsp;ຕິດຕໍ່</a>&nbsp;
                 <a href="https://wa.me/+8562076589225" class="btn btn-success"><i class="fa fa-whatsapp"></i>&nbsp;Whatsapp</a>&nbsp;
                 <a href="https://www.facebook.com/profile.php?id=100079975531285" class="btn btn-primary"><i class="fa fa-facebook"></i>&nbsp;Facebook</a>&nbsp;
-                <a href="mailto:kangserpobtsuasvaaj@gmail.com" class="btn btn-primary"><i class="fa fa-envelop"></i>&nbsp;Mail</a>&nbsp;
+                <a href="mailto:kangserpobtsuasvaaj@gmail.com" class="btn btn-primary"><i class="fas fa-envelope"></i>&nbsp;Mail</a>&nbsp;
                 <table class="table table-striped">
                   <thead>
                     <tr>
